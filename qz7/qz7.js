cb7270312548392df1922032f8277b696f2cf97d
zfong <100269395+zfong@users.noreply.github.com>
2022-04-27 17:01:15 +0800

    Add files via upload

---
+++ qz7/qz7.js
@@ -0,0 +1,33 @@
+var rain_data=[
+{
+      stationNo C0AD1,
+       stationName 八里,
+       recTime 202204271600,
+       rain 0
+},
+{
+       stationNo C0AD0,
+       stationName 三芝,
+       recTime 202204271600,
+       rain 0
+},
+{
+       stationNo C0AD4,
+       stationName 土城,
+       recTime 202204271600,
+       rain 0
+},
+{
+       stationNo 318,
+       stationName 大安福州山,
+       recTime 202204271602,
+       rain 0
+},
+{
+       stationNo 01A17,
+       stationName 坪林,
+       recTime 201911130140,
+       rain 0
+},
+  
+]
